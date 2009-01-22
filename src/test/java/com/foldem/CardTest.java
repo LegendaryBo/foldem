4eb78333e4cb9740616c229896ea9dc7973bb54b
robert.strack <robert.strack@f9e5b33a-e8b5-11dd-a550-c997509ed985>
2009-01-22 20:41:38 +0000

    added tests

---
+++ src/test/java/com/foldem/CardTest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2009 Robert Strack
+ *
+ * This file is part of Foldem.
+ *
+ * Foldem is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * code_swarm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foldem.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.foldem;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CardTest {
+
+	@Test
+	public void testNames() throws Exception {
+		for (Card card : Card.values()) {
+			assertEquals(card.getRank().name() + "_" + card.getSuit().name(), card.name());
+			assertEquals(card.getRank().toString() + card.getSuit().toString(), card.toString());
+		}
+	}
+
+}
